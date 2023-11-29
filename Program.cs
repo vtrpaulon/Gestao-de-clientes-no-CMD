95504dcf71e6ca0265a130748afcb00e72bd9e4e
VitorPaulon <71847592+vtrpaulon@users.noreply.github.com>
2023-11-29 06:19:44 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,111 +1,121 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
 
-class Program
+namespace Projeto2
 {
-    [System.Serializable]
-
-    struct Cliente
+    class Program
     {
-        public string nome;
-        public string email;
-        public string cpf;
-    }
-    static List<Cliente> clientes = new List<Cliente>();
 
-    enum Menu { Listagem = 1, Adicionar = 2, Remover = 3, Sair = 4 }
-    public static void Main(string[] args)
-    {
-        Carregar();
-        bool escolheuSair = false;
-        while (!escolheuSair)
+        [System.Serializable]
+        struct Cliente
         {
-            Console.WriteLine("Bem vindo ao sistema!");
-            Console.WriteLine(" 1 - Listagem \n 2 - Adicionar \n 3 - Remover \n 4 - Sair");
-            int intOpcao = int.Parse(Console.ReadLine());
-            Menu opcao = (Menu)intOpcao;
-
-            switch (opcao)
-            {
-                case Menu.Adicionar:
-                    Adicionar();
-                    break;
-                case Menu.Listagem:
-                    Listagem();
-                    break;
-                case Menu.Remover:
-                    Remover();
-                    break;
-                case Menu.Sair:
-                    escolheuSair = true;
-                    Console.WriteLine("***** Fechando sistema!!! *****");
-                    break;
-            }
-            Console.Clear();
+            public string nome;
+            public string email;
+            public string cpf;
         }
+
+        static List<Cliente> clientes = new List<Cliente>();
+
+        enum Menu { Listagem = 1, Adicionar = 2 , Remover = 3, Sair = 4}
+
+        static void Main(string[] args)
+        {
+            Carregar();
+            bool escolheuSair = false;
+            while (!escolheuSair)
+            {
+                Console.WriteLine("Sistema de clientes - Bem vindo!");
+                Console.WriteLine("1-Listagem\n2-Adicionar\n3-Remover\n4-Sair");
+                int intOp = int.Parse(Console.ReadLine());
+                Menu opcao = (Menu)intOp;
+
+                switch (opcao)
+                {
+                    case Menu.Adicionar:
+                        Adicionar();
+                        break;
+                    case Menu.Listagem:
+                        Listagem();
+                        break;
+                    case Menu.Remover:
+                        Remover();
+                        break;
+                    case Menu.Sair:
+                        escolheuSair = true;
+                        break;
+                }
+                Console.Clear();
+            }
+        }
+
 
         static void Adicionar()
         {
             Cliente cliente = new Cliente();
-            Console.WriteLine("Cadastro de clientes: ");
-
+            Console.WriteLine("Cadastro de cliente: ");
             Console.WriteLine("Nome do cliente: ");
             cliente.nome = Console.ReadLine();
             Console.WriteLine("Email do cliente: ");
             cliente.email = Console.ReadLine();
-            Console.WriteLine("CPF do cliente: ");
+            Console.WriteLine("CPF do cliente:");
             cliente.cpf = Console.ReadLine();
-            
 
-            Console.Add(cliente);
+            clientes.Add(cliente);
             Salvar();
-
-            Console.WriteLine("Cadastro concluido! Aperte ENTER para sair");
+            Console.WriteLine("Cadastro concluído, aperte enter para sair.");
             Console.ReadLine();
         }
 
         static void Listagem()
         {
-            if(clientes.Count > 0)
+
+            if(clientes.Count > 0) // SE tem pelo menos um cliente
             {
                 Console.WriteLine("Lista de clientes: ");
-                int i=0;
-                foreach(Cliente cliente in clientes)
+                int i = 0;
+                foreach (Cliente cliente in clientes)
                 {
                     Console.WriteLine($"ID: {i}");
                     Console.WriteLine($"Nome: {cliente.nome}");
-                    Console.WriteLine($"Email: {cliente.email}");
+                    Console.WriteLine($"E-mail: {cliente.email}");
                     Console.WriteLine($"CPF: {cliente.cpf}");
-                    Console.WriteLine("**************************************");
+                    Console.WriteLine("====================================");
                     i++;
+                   
                 }
             }
             else
             {
-                Console.WriteLine("Nenhum cliente cadastrado.")
+                Console.WriteLine("Nenhum cliente cadastrado!");
             }
-            Console.WriteLine("Aperte ENTER para sair");
+
+
+            Console.WriteLine("Aperte enter para sair.");
             Console.ReadLine();
         }
 
         static void Remover()
         {
             Listagem();
-            Console.WriteLine("Digite o ID do cliente que voce quer remover");
+            Console.WriteLine("Digite o ID do cliente que você quer remover:");
             int id = int.Parse(Console.ReadLine());
-
-            if(id >= 0 && id < clientes.Count)
+            if (id >= 0 && id < clientes.Count)
             {
+
                 clientes.RemoveAt(id);
                 Salvar();
 
             }
             else
             {
-                Console.WriteLine("O id digitado é invalido, tente novamente!");
+                Console.WriteLine("Id digitado é inválido, tente novamente!");
                 Console.ReadLine();
             }
-
         }
 
         static void Salvar()
@@ -114,14 +124,16 @@ class Program
             BinaryFormatter enconder = new BinaryFormatter();
 
             enconder.Serialize(stream, clientes);
+
             stream.Close();
         }
+
         static void Carregar()
         {
-            FileStream stream = new FileStream("clientes.dat",FileMode.OpenOrCreate);
+            FileStream stream = new FileStream("clientes.dat", FileMode.OpenOrCreate);
 
             try
-            {
+            {            
                 BinaryFormatter enconder = new BinaryFormatter();
 
                 clientes = (List<Cliente>)enconder.Deserialize(stream);
@@ -129,16 +141,15 @@ class Program
                 if(clientes == null)
                 {
                     clientes = new List<Cliente>();
-                }
+                }          
             }
             catch(Exception e)
             {
                 clientes = new List<Cliente>();
             }
-            finally
-            {
-                stream.Close();
-            }
+
+
+            stream.Close();
         }
 
     }
